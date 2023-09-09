07e0e53fce43bd666c671181532b27500c43af7e
A.J. Marchese <aj.marchese@outlook.com>
2023-09-09 13:31:54 -0700

    added mouse over method

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
+  onCountryHover(event: MouseEvent) {
+    let countryElement = event.target as SVGPathElement;
+    let countryId = countryElement.id || countryElement.getAttribute('class');
 
+    if (countryId) {
+      this.fetchCountryData(countryId); // or your method to fetch data
+    }
+  }
+
+  fetchCountryData(countryId: string) {
+    // Your logic to fetch data goes here, likely involving the GeoApiService you created earlier
+  }
 }
