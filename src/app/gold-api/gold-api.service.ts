c7767cac0a7c837c9bcfd37c16f554378c7b6e2f
Andrew J Marchese <aj.marchese@outlook.com>
2023-09-19 22:07:21 -0700

    added gold api ticker

--- src/app/gold-api/gold-api.service.ts
+++ src/app/gold-api/gold-api.service.ts
@@ -1,21 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoldApiService {
 
-  private apiUrl = 'https://www.goldapi.io/api/XAU/GBP';
-  private headers = new HttpHeaders({
-    'x-access-token': '**************',
-    'Content-Type': 'application/json'
-  });
+  private apiUrl = 'https://www.goldapi.io/api/XAU/USD';
 
   constructor(private http: HttpClient) { }
 
+  private fetchApiKey(): Observable<string> {
+    return this.http.get('assets/api-keys.json').pipe(
+      map((config: any) => config.GOLD_API_KEY)
+    );
+  }
+
   fetchGoldData(): Observable<any> {
-    return this.http.get(this.apiUrl, { headers: this.headers });
+    return this.fetchApiKey().pipe(
+      switchMap(apiKey => {
+        const headers = new HttpHeaders({
+          'x-access-token': apiKey,
+          'Content-Type': 'application/json'
+        });
+        return this.http.get(this.apiUrl, { headers: headers });
+      })
+    );
   }
 }
--- src/app/ticker/ticker.component.ts
+++ src/app/ticker/ticker.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoldApiService } from '../gold-api/gold-api.service';
 
 @Component({
   selector: 'app-ticker',
   templateUrl: './ticker.component.html',
   styleUrls: ['./ticker.component.css']
 })
-export class TickerComponent {
+export class TickerComponent implements OnInit { // Implement the OnInit interface
 
+  public goldData: any;
+
+  constructor(private goldApiService: GoldApiService) { }
+
+  fetchGoldData() {
+    this.goldApiService.fetchGoldData().subscribe(
+      data => {
+        console.log('Gold API Data:', data);
+        this.goldData = data;
+      },
+      error => console.log('error', error)
+    );
+  }
+
+  ngOnInit() {  // Define the ngOnInit method
+    this.fetchGoldData();
+  }
 }
+
